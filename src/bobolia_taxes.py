86355f46b433e2f3553d3970bfbe57a8cf99a5ae
Robert C. Martin <unclebob@cleancoder.com>
2024-08-11 09:55:47 -0500

    BadBobAdjuster extracted

--- src/bobolia_taxes.py
+++ src/bobolia_taxes.py
@@ -6,22 +6,8 @@ class TaxCalculator:
     def get_tax(self, tax_return):
         self.tax_return = tax_return
         tax = self.determine_base_tax()
-        tax += self.determine_badbob_adjustment()
+        tax += BadBobAdjuster(self).get_adjustment()
         return tax
-
-    def determine_badbob_adjustment(self):
-        class1 = sum(self.tax_return["badbobs"]["class1"])
-        class1_adjustment = class1 /2
-        class2 = sum(self.tax_return["badbobs"]["class2"])
-        if class2<1001:
-            class2_adjustment = 0
-        elif class2<10001:
-            class2_adjustment = self.total_income * 0.05
-        elif class2<50001:
-            class2_adjustment = self.total_income * 0.1
-        else:
-            class2_adjustment = self.total_income * 0.15
-        return class1_adjustment + class2_adjustment
 
     def determine_base_tax(self):
         self.total_income = self.tax_return["income"]["salary"]
@@ -30,3 +16,36 @@ class TaxCalculator:
         if after_tax_income < 30000:
             tax = self.total_income - 30000
         return tax
+
+
+class BadBobAdjuster:
+    def __init__(self, tax_calculator):
+        self.tax_calculator = tax_calculator
+
+    def get_adjustment(self):
+        class1_adjustment = self.class1_adjustment()
+        class2_adjustment = self.get_class2_badbob_adjustment()
+        return class1_adjustment + class2_adjustment
+
+    def get_class2_badbob_adjustment(self):
+        class2 = sum(self.tax_calculator.tax_return["badbobs"]["class2"])
+        class2_rate = self.get_class2_rate(class2)
+
+        return class2_rate * self.tax_calculator.total_income
+
+    def get_class2_rate(self, class2):
+        class2_rate = 0
+        class2_table = ((1001, 0),
+                        (10001, 0.05),
+                        (50001, 0.1),
+                        ("max", 0.15))
+        for row in class2_table:
+            if row[0] == "max" or class2 < row[0]:
+                class2_rate = row[1]
+                break
+        return class2_rate
+
+    def class1_adjustment(self):
+        class1 = sum(self.tax_calculator.tax_return["badbobs"]["class1"])
+        class1_adjustment = class1 / 2
+        return class1_adjustment
